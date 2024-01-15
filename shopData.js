20fe636124ecc19bcc360b9f58485b5e5d2b68bc
sbn raju <simhardrinagaraju@gmail.com>
2024-01-15 20:58:10 +0530

    Raw Data

---
+++ shopData.js
@@ -0,0 +1,171 @@
+let itemsData = [
+    {
+        item_pic:"https://m.media-amazon.com/images/I/616yvupE77L._AC_UL480_QL65_.jpg",
+        item_name:"Ceat Milaze 90/100-10 53J Tubeless Scooter Tyre,Front or Rear",
+        item_company:"CEAT MILAZE",
+        item_category:"Tyre",
+        item_price:2088,
+        item_discount:48,
+        item_rating:4,
+        item_des:"Vehicle Compatibility (Brand/Model) - Hero Ez, Hero Maestro, Hero Pleasure, Honda Activa, Honda Dio, Piaggio Vespa, Suzuki Access, Suzuki Swish, Yamaha Alpha, Yamaha Fascino, Yamaha Ray"
+                  
+    },
+    {
+        item_pic:"https://m.media-amazon.com/images/I/71k+ny04DcL._AC_UL480_QL65_.jpg",
+        item_name:"Bridgestone S248 TL 165/80 R14 85T Tubeless Car Tyre",
+        item_company:"Bridgestone",
+        item_category:"Car Tyre",
+        item_price:4427,
+        item_discount:46,
+        item_rating:4,
+        item_des:"Vehicle compatibility: Opel Astra / Maruti Swift Lxi, Swift Vxi / Fiat Premier Padmini"
+        
+                  
+    },
+    {
+        item_pic:"https://m.media-amazon.com/images/I/81u07KntP1L._AC_UL480_QL65_.jpg",
+        item_name:"Motul Scooter LE 10W30 Engine Oil (800 ml)",
+        item_company:"Motul",
+        item_category:"Engine Oil",
+        item_price:345,
+        item_discount:9,
+        item_rating:4,
+        item_des:"Vehicle Compatibility (Brand/Model) - Hero Ez, Hero Maestro, Hero Pleasure, Honda Activa, Honda Dio, Piaggio Vespa, Suzuki Access, Suzuki Swish, Yamaha Alpha, Yamaha Fascino, Yamaha Ray"
+                  
+    },
+    {
+        item_pic:"https://m.media-amazon.com/images/I/71jr7yBbOVL._AC_UL480_QL65_.jpg",
+        item_name:"Castrol Activ 20W-40 4T Synthetic Engine Oil for Bikes 1L |3X Protection | with Actibond Technology | Engine Protection for Bikes | JASO MA2",
+        item_company:"Castrol",
+        item_category:"Engine Oil",
+        item_price:442,
+        item_discount:13,
+        item_rating:4,
+        item_des:"Vehicle Compatibility (Brand/Model) - Hero Ez, Hero Maestro, Hero Pleasure, Honda Activa, Honda Dio, Piaggio Vespa, Suzuki Access, Suzuki Swish, Yamaha Alpha, Yamaha Fascino, Yamaha Ray"
+                  
+    },
+    {
+        item_pic:"https://m.media-amazon.com/images/I/71ca7JJvF-L._AC_UL480_QL65_.jpg",
+        item_name:"Shell Helix HX8 5W-30 API SN Plus Fully Synthetic Engine Oil for Petrol, Diesel & CNG Cars (3.5 L), medium",
+        item_company:"Shell",
+        item_category:"Engine Oil",
+        item_price:2604,
+        item_discount:30,
+        item_rating:4,
+        item_des:"Vehicle Compatibility (Brand/Model) - Hero Ez, Hero Maestro, Hero Pleasure, Honda Activa, Honda Dio, Piaggio Vespa, Suzuki Access, Suzuki Swish, Yamaha Alpha, Yamaha Fascino, Yamaha Ray"
+                  
+    },
+    {
+        item_pic:"https://m.media-amazon.com/images/I/616yvupE77L._AC_UL480_QL65_.jpg",
+        item_name:"Ceat Milaze 90/100-10 53J Tubeless Scooter Tyre,Front or Rear",
+        item_company:"CEAT MILAZE",
+        item_category:"Tyre",
+        item_price:2088,
+        item_discount:48,
+        item_rating:4,
+        item_des:"Vehicle Compatibility (Brand/Model) - Hero Ez, Hero Maestro, Hero Pleasure, Honda Activa, Honda Dio, Piaggio Vespa, Suzuki Access, Suzuki Swish, Yamaha Alpha, Yamaha Fascino, Yamaha Ray"
+                  
+    },
+    {
+        item_pic:"https://m.media-amazon.com/images/I/61rbRoGlfvL._AC_UL480_QL65_.jpg",
+        item_name:"Castrol POWER1 CRUISE 20W-50 4T Synthetic Engine Oil for Bikes 1L",
+        item_company:"Castrol",
+        item_category:"Engine Oil",
+        item_price:556,
+        item_discount:16,
+        item_rating:4,
+        item_des:"Vehicle Compatibility (Brand/Model) - Hero Ez, Hero Maestro, Hero Pleasure, Honda Activa, Honda Dio, Piaggio Vespa, Suzuki Access, Suzuki Swish, Yamaha Alpha, Yamaha Fascino, Yamaha Ray"
+                  
+    },
+    {
+        item_pic:"https://m.media-amazon.com/images/I/51PQYbQWiSL.AC_SX250.jpg",
+        item_name:"Neodrift 'SilverMax' Bike Cover for BMW G310GS (All-Weather Motorcycle Protection, Water & UV Resistant, Dustproof, Windproof).",
+        item_company:"Neodrift",
+        item_category:"Vehicle Cover",
+        item_price:1900,
+        item_discount:90,
+        item_rating:4,
+        item_des:"Vehicle Compatibility (Brand/Model) - Hero Ez, Hero Maestro, Hero Pleasure, Honda Activa, Honda Dio, Piaggio Vespa, Suzuki Access, Suzuki Swish, Yamaha Alpha, Yamaha Fascino, Yamaha Ray"
+                  
+    },
+    {
+        item_pic:"https://m.media-amazon.com/images/I/616yvupE77L._AC_UL480_QL65_.jpg",
+        item_name:"Ceat Milaze 90/100-10 53J Tubeless Scooter Tyre,Front or Rear",
+        item_company:"CEAT MILAZE",
+        item_category:"Tyre",
+        item_price:2088,
+        item_discount:48,
+        item_rating:4,
+        item_des:"Vehicle Compatibility (Brand/Model) - Hero Ez, Hero Maestro, Hero Pleasure, Honda Activa, Honda Dio, Piaggio Vespa, Suzuki Access, Suzuki Swish, Yamaha Alpha, Yamaha Fascino, Yamaha Ray"
+                  
+    },
+    {
+        item_pic:"https://m.media-amazon.com/images/I/51zdCp557LL._SX300_SY300_QL70_FMwebp_.jpg",
+        item_name:"NEODRIFT 'SilverTech' Car Cover for MG Astor (100% Water Resistant, Tailored Fit, All-Weather Protection, Multi-Layered & Breathable Fabric) Colour - Silver",
+        item_company:"NeoDrift",
+        item_category:"Vechile cover",
+        item_price:3600,
+        item_discount:50,
+        item_rating:4,
+        item_des:"Vehicle Compatibility (Brand/Model) - Hero Ez, Hero Maestro, Hero Pleasure, Honda Activa, Honda Dio, Piaggio Vespa, Suzuki Access, Suzuki Swish, Yamaha Alpha, Yamaha Fascino, Yamaha Ray"
+                  
+    },
+    {
+        item_pic:"https://m.media-amazon.com/images/I/616yvupE77L._AC_UL480_QL65_.jpg",
+        item_name:"Ceat Milaze 90/100-10 53J Tubeless Scooter Tyre,Front or Rear",
+        item_company:"CEAT MILAZE",
+        item_category:"Tyre",
+        item_price:2088,
+        item_discount:48,
+        item_rating:4,
+        item_des:"Vehicle Compatibility (Brand/Model) - Hero Ez, Hero Maestro, Hero Pleasure, Honda Activa, Honda Dio, Piaggio Vespa, Suzuki Access, Suzuki Swish, Yamaha Alpha, Yamaha Fascino, Yamaha Ray"
+                  
+    },
+    {
+        item_pic:"https://m.media-amazon.com/images/I/616yvupE77L._AC_UL480_QL65_.jpg",
+        item_name:"Ceat Milaze 90/100-10 53J Tubeless Scooter Tyre,Front or Rear",
+        item_company:"CEAT MILAZE",
+        item_category:"Tyre",
+        item_price:2088,
+        item_discount:48,
+        item_rating:4,
+        item_des:"Vehicle Compatibility (Brand/Model) - Hero Ez, Hero Maestro, Hero Pleasure, Honda Activa, Honda Dio, Piaggio Vespa, Suzuki Access, Suzuki Swish, Yamaha Alpha, Yamaha Fascino, Yamaha Ray"
+                  
+    },
+    {
+        item_pic:"https://m.media-amazon.com/images/I/616yvupE77L._AC_UL480_QL65_.jpg",
+        item_name:"Ceat Milaze 90/100-10 53J Tubeless Scooter Tyre,Front or Rear",
+        item_company:"CEAT MILAZE",
+        item_category:"Tyre",
+        item_price:2088,
+        item_discount:48,
+        item_rating:4,
+        item_des:"Vehicle Compatibility (Brand/Model) - Hero Ez, Hero Maestro, Hero Pleasure, Honda Activa, Honda Dio, Piaggio Vespa, Suzuki Access, Suzuki Swish, Yamaha Alpha, Yamaha Fascino, Yamaha Ray"
+                  
+    },
+    {
+        item_pic:"https://m.media-amazon.com/images/I/616yvupE77L._AC_UL480_QL65_.jpg",
+        item_name:"Ceat Milaze 90/100-10 53J Tubeless Scooter Tyre,Front or Rear",
+        item_company:"CEAT MILAZE",
+        item_category:"Tyre",
+        item_price:2088,
+        item_discount:48,
+        item_rating:4,
+        item_des:"Vehicle Compatibility (Brand/Model) - Hero Ez, Hero Maestro, Hero Pleasure, Honda Activa, Honda Dio, Piaggio Vespa, Suzuki Access, Suzuki Swish, Yamaha Alpha, Yamaha Fascino, Yamaha Ray"
+                  
+    },
+    {
+        item_pic:"https://m.media-amazon.com/images/I/616yvupE77L._AC_UL480_QL65_.jpg",
+        item_name:"Ceat Milaze 90/100-10 53J Tubeless Scooter Tyre,Front or Rear",
+        item_company:"CEAT MILAZE",
+        item_category:"Tyre",
+        item_price:2088,
+        item_discount:48,
+        item_rating:4,
+        item_des:"Vehicle Compatibility (Brand/Model) - Hero Ez, Hero Maestro, Hero Pleasure, Honda Activa, Honda Dio, Piaggio Vespa, Suzuki Access, Suzuki Swish, Yamaha Alpha, Yamaha Fascino, Yamaha Ray"
+                  
+    },
+    
+];
+
+module.exports = itemsData;
