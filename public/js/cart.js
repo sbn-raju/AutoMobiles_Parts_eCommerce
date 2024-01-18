179861d2b578ddcb9feabb0719da8f67d320bdef
sbn raju <simhardrinagaraju@gmail.com>
2024-01-18 15:46:21 +0530

    Added Responsivnes

---
+++ public/js/cart.js
@@ -0,0 +1,11 @@
+// import axios from "axios";
+// let btns = document.querySelectorAll(".remove-from-cart-btn");
+// for(let btn of btns){
+//     btn.addEventListener("click",()=>{
+//         axios.get("https://localhost:8080/cart/count").then((result)=>{
+//             console.log("Button Clicked succesfull");
+//         }).catch((err)=>{
+//             console.log(err);
+//         });
+//     });
+// }
